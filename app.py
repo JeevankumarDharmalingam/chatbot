e42c7f1c9b941cbc8eea5ad7910777e13ac580a0
JeevankumarDharmalingam <40886674+JeevankumarDharmalingam@users.noreply.github.com>
2024-05-09 15:14:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
-from langchain_openai import ChatOpenAI,AzureChatOpenAI,AzureOpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-import streamlit as st
-import os
-from dotenv import load_dotenv
 
-# os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-# ## Langmith tracking
-# os.environ["LANGCHAIN_TRACING_V2"]="true"
-# os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+import openai
+import gradio as gr
 
-## Prompt Template
 
-prompt=ChatPromptTemplate.from_messages(
-    [
-        ("system","You are a helpful assistant. Please response to the user queries"),
-        ("user","Question:{question}")
-    ]
-)
+client = openai.AzureOpenAI(
+      azure_endpoint = "https://caedaoipocaoa0c.openai.azure.com",
+      api_key="d8bd990c34974f8fa34c5e0fe1a37483", # api_key is required, but unused for local models
+      api_version="2023-03-15-preview", # version
+      
+      
+  )
 
-## streamlit framework
+def predict(message, history):
+    history_openai_format = []
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant", "content":assistant})
+    history_openai_format.append({"role": "user", "content": message})
+  
+    response = client.chat.completions.create(model='DDIAIGPT4',
+    messages= history_openai_format,
+    temperature=1.0,
+    stream=True)
 
-st.title('Langchain Demo With OPENAI API')
-input_text=st.text_input("Search the topic u want")
+    partial_message = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+              partial_message = partial_message + chunk.choices[0].delta.content
+              yield partial_message
 
-# openAI LLm 
-llm=AzureChatOpenAI(
-    streaming=False,
-    openai_api_key="d8bd990c34974f8fa34c5e0fe1a37483",
-    azure_endpoint="https://caedaoipocaoa0c.openai.azure.com",
-    openai_api_type="azure",
-    openai_api_version="2023-03-15-preview",
-    deployment_name="DDIAIGPT4"
-)
-output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
-
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
+gr.ChatInterface(predict).launch()
